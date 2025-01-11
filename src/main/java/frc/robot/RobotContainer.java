ee5588f2aaaa444be58687d53e0574b26d9ed4ca
Mr-A768 <micah.a768@gmail.com>
2025-01-11 15:28:04 -0600

    why the dean did none of the changes to main get applied to this branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -31,8 +31,13 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 public class RobotContainer {
   private LinearVelocity MaxSpeed = TunerConstants.kSpeedAt12Volts;
-  private final TunableController joystick =
-      new TunableController(0).withControllerType(TunableControllerType.QUADRATIC);
+  private final TunableController driver =
+      new TunableController(0)
+          .withControllerType(TunableControllerType.QUADRATIC)
+          .withOutputAtDeadband(0.025)
+          .withDeadband(0.125);
+
+  private final TunableController reefTargetingSystem = new TunableController(2);
 
   private final LoggedDashboardChooser<Command> autoChooser;
 
@@ -40,8 +45,8 @@ public class RobotContainer {
   // CTRE Default Drive Request
   private final SwerveRequest.FieldCentric drive =
       new SwerveRequest.FieldCentric()
-          .withDeadband(MaxSpeed.times(0.1))
-          .withRotationalDeadband(Constants.MaxAngularRate.times(0.1)) // Add a 10% deadband
+          .withDeadband(MaxSpeed.times(0.025))
+          .withRotationalDeadband(Constants.MaxAngularRate.times(0.025)) // Add a 10% deadband
           .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
   /* Setting up bindings for necessary control of the swerve drive platform */
@@ -138,26 +143,24 @@ public class RobotContainer {
                 drive
                     .withVelocityX(
                         MaxSpeed.times(
-                            -joystick
-                                .customLeft()
-                                .getY())) // Drive forward with negative Y (forward)
+                            -driver.customLeft().getY())) // Drive forward with negative Y (forward)
                     .withVelocityY(
                         MaxSpeed.times(
-                            -joystick.customLeft().getX())) // Drive left with negative X (left)
+                            -driver.customLeft().getX())) // Drive left with negative X (left)
                     .withRotationalRate(
                         Constants.MaxAngularRate.times(
-                            -joystick
+                            -driver
                                 .customRight()
                                 .getX())))); // Drive counterclockwise with negative X (left)
 
-    joystick.a().onTrue(Commands.runOnce(() -> drivetrain.resetPose(Pose2d.kZero)));
-    joystick
+    driver.a().onTrue(Commands.runOnce(() -> drivetrain.resetPose(Pose2d.kZero)));
+    driver
         .b()
         .whileTrue(
             drivetrain.applyRequest(
                 () ->
                     point.withModuleDirection(
-                        new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+                        new Rotation2d(-driver.getLeftY(), -driver.getLeftX()))));
 
     // Custom Swerve Request that use PathPlanner Setpoint Generator. Tuning NEEDED. Instructions
     // can be found here
@@ -170,7 +173,7 @@ public class RobotContainer {
             .withDeadband(MaxSpeed.times(0.1))
             .withRotationalDeadband(Constants.MaxAngularRate.times(0.1))
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
-    joystick
+    driver
         .x()
         .whileTrue(
             drivetrain.applyRequest(
@@ -178,9 +181,9 @@ public class RobotContainer {
                     setpointGen
                         .withVelocityX(
                             MaxSpeed.times(
-                                -joystick.getLeftY())) // Drive forward with negative Y (forward)
-                        .withVelocityY(MaxSpeed.times(-joystick.getLeftX()))
-                        .withRotationalRate(Constants.MaxAngularRate.times(-joystick.getRightX()))
+                                -driver.getLeftY())) // Drive forward with negative Y (forward)
+                        .withVelocityY(MaxSpeed.times(-driver.getLeftX()))
+                        .withRotationalRate(Constants.MaxAngularRate.times(-driver.getRightX()))
                         .withOperatorForwardDirection(drivetrain.getOperatorForwardDirection())));
 
     // Custom Swerve Request that use ProfiledFieldCentricFacingAngle. Allows you to face specific
@@ -194,7 +197,7 @@ public class RobotContainer {
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
     // Set PID for ProfiledFieldCentricFacingAngle
     driveFacingAngle.HeadingController.setPID(7, 0, 0);
-    joystick
+    driver
         .y()
         .whileTrue(
             drivetrain
@@ -205,22 +208,32 @@ public class RobotContainer {
                             driveFacingAngle
                                 .withVelocityX(
                                     MaxSpeed.times(
-                                        -joystick
+                                        -driver
                                             .getLeftY())) // Drive forward with negative Y (forward)
-                                .withVelocityY(MaxSpeed.times(-joystick.getLeftX()))
+                                .withVelocityY(MaxSpeed.times(-driver.getLeftX()))
                                 .withTargetDirection(
-                                    new Rotation2d(
-                                        -joystick.getRightY(), -joystick.getRightX())))));
+                                    new Rotation2d(-driver.getRightY(), -driver.getRightX())))));
 
     // Run SysId routines when holding back/start and X/Y.
     // Note that each routine should be run exactly once in a single log.
-    joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-    joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-    joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-    joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+    driver.rightBumper().and(driver.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+    driver.rightBumper().and(driver.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+    driver.leftBumper().and(driver.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+    driver.leftBumper().and(driver.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
     // reset the field-centric heading on left bumper press
-    // joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+    driver
+        .start()
+        .onTrue(
+            drivetrain.runOnce(
+                () ->
+                    drivetrain.resetPose(
+                        new Pose2d(
+                            drivetrain.getPose().getX(),
+                            drivetrain.getPose().getY(),
+                            new Rotation2d()))));
+
+    reefTargetingSystem.a();
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/utils/TargetingComputer.java
+++ src/main/java/frc/robot/utils/TargetingComputer.java
@@ -1,54 +1,59 @@
 package frc.robot.utils;
 
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-
 public enum TargetingComputer {
-    RED_ALPHA (7, 180, "left"),
-    RED_BRAVO (7, 180, "right"),
-    RED_CHARLIE (8, 240, "left"),
-    RED_DELTA (8, 240, "right"),
-    RED_ECHO (9, 300, "left"),
-    RED_FOXTROT (9, 300, "right"),
-    RED_GOLF (10, 0, "left"),
-    RED_HOTEL (10, 0, "right"),
-    RED_INDIA (11, 60, "left"),
-    RED_JULIET (11, 60, "right"),
-    RED_KILO (6, 120, "left"),
-    RED_LIMA (6, 120, "right"),
-    RED_SOURCE_LEFT (1, 306, null),
-    RED_SOURCE_RIGHT (2, 54, null),
-    RED_PROCESSOR (3, 90, null),
-    RED_NET (5, 180, null),
+  RED_ALPHA(7, 180, "left"),
+  RED_BRAVO(7, 180, "right"),
+  RED_CHARLIE(8, 240, "left"),
+  RED_DELTA(8, 240, "right"),
+  RED_ECHO(9, 300, "left"),
+  RED_FOXTROT(9, 300, "right"),
+  RED_GOLF(10, 0, "left"),
+  RED_HOTEL(10, 0, "right"),
+  RED_INDIA(11, 60, "left"),
+  RED_JULIET(11, 60, "right"),
+  RED_KILO(6, 120, "left"),
+  RED_LIMA(6, 120, "right"),
+  RED_SOURCE_LEFT(1, 306, null),
+  RED_SOURCE_RIGHT(2, 54, null),
+  RED_PROCESSOR(3, 90, null),
+  RED_NET(5, 180, null),
 
-    BLUE_ALPHA (18, 0, "left"),
-    BLUE_BRAVO (18, 0, "right"),
-    BLUE_CHARLIE (17, 60, "left"),
-    BLUE_DELTA (17, 60, "right"),
-    BLUE_ECHO (22, 120, "left"),
-    BLUE_FOXTROT (22, 120, "right"),
-    BLUE_GOLF (21, 180, "left"),
-    BLUE_HOTEL (21, 180, "right"),
-    BLUE_INDIA (20, 240, "left"),
-    BLUE_JULIET (20, 240, "right"),
-    BLUE_KILO (19, 300, "left"),
-    BLUE_LIMA (19, 300, "right"),
-    BLUE_SOURCE_LEFT (13, 126, null),
-    BLUE_SOURCE_RIGHT (12, 234, null),
-    BLUE_PROCESSOR (16, 270, null),
-    BLUE_NET (14, 0, null);
+  BLUE_ALPHA(18, 0, "left"),
+  BLUE_BRAVO(18, 0, "right"),
+  BLUE_CHARLIE(17, 60, "left"),
+  BLUE_DELTA(17, 60, "right"),
+  BLUE_ECHO(22, 120, "left"),
+  BLUE_FOXTROT(22, 120, "right"),
+  BLUE_GOLF(21, 180, "left"),
+  BLUE_HOTEL(21, 180, "right"),
+  BLUE_INDIA(20, 240, "left"),
+  BLUE_JULIET(20, 240, "right"),
+  BLUE_KILO(19, 300, "left"),
+  BLUE_LIMA(19, 300, "right"),
+  BLUE_SOURCE_LEFT(13, 126, null),
+  BLUE_SOURCE_RIGHT(12, 234, null),
+  BLUE_PROCESSOR(16, 270, null),
+  BLUE_NET(14, 0, null);
 
-    private final double apriltag;
-    private final double targetingAngle; // in deg
-    private final String side;
+  private final double apriltag;
+  private final double targetingAngle; // in deg
+  private final String side;
 
-    TargetingComputer(double apriltag, double targetingAngle, String side) {
-        this.apriltag = apriltag;
-        this.targetingAngle = targetingAngle;
-        this.side = side;
-    }
+  TargetingComputer(double apriltag, double targetingAngle, String side) {
+    this.apriltag = apriltag;
+    this.targetingAngle = targetingAngle;
+    this.side = side;
+  }
 
-    public double getApriltag() {return apriltag;}
-    public double getTargetingAngle() {return targetingAngle;}
-    public String getSide() {return side;}
+  public double getApriltag() {
+    return apriltag;
+  }
+
+  public double getTargetingAngle() {
+    return targetingAngle;
+  }
+
+  public String getSide() {
+    return side;
+  }
 }
