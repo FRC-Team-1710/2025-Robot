341cc880210c3384f85e627713538c24cf59341a
Mr-A768 <micah.a768@gmail.com>
2025-03-06 18:13:14 -0600

    broke my laptop on the thethered practice field 💔

--- src/main/java/frc/robot/commands/Climb.java
+++ src/main/java/frc/robot/commands/Climb.java
@@ -10,7 +10,7 @@ import frc.robot.subsystems.superstructure.climber.Climber;
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class Climb extends Command {
   Climber climber;
-  double targetRotations = 5.35;
+  double targetRotations = 5.3;
   /** Creates a new Climb. */
   public Climb(Climber climber) {
     this.climber = new Climber();
