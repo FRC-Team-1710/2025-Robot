4fd4890f9abd55f82a015a8ede033af2b5b47469
Mr-A768 <micah.a768@gmail.com>
2025-04-10 18:30:43 -0500

    DEAN!!! RELEASE DIVISIONS NOW AND MY LIFE IS YOURS!!!

--- src/main/java/frc/robot/subsystems/superstructure/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/superstructure/elevator/Elevator.java
@@ -110,8 +110,8 @@ public class Elevator extends SubsystemBase {
     L2(Inches.of(15.75)), // Position for scoring in L2
     L3(Inches.of(32.25)), // Position for scoring in L3
     L4(Inches.of(54.5)), // Position for scoring in L4
-    ALGAE_LOW(Inches.of(10), Inches.of(1.5)), // Position for grabbing low algae
-    ALGAE_HIGH(Inches.of(26.5), Inches.of(1.5)); // Position for grabbing high algae
+    ALGAE_LOW(Inches.of(19), Inches.of(1.5)), // Position for grabbing low algae
+    ALGAE_HIGH(Inches.of(25.5), Inches.of(1.5)); // Position for grabbing high algae
 
     private final Distance targetDistance;
     private final Distance angleTolerance;
