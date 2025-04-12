7df91bc78da6a1cf8b7fbf08d19489aef0d6f393
Mr-A768 <micah.a768@gmail.com>
2025-04-11 19:04:14 -0500

    new drive pid

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -188,7 +188,7 @@ public class Drive extends SubsystemBase {
               this));
 
   /* The SysId routine to test */
-  private SysIdRoutine m_sysIdRoutineToApply = m_sysIdRoutineTranslation;
+  private SysIdRoutine m_sysIdRoutineToApply = m_sysIdRoutineRotation;
 
   // logging
   private Field2d m_field = new Field2d();
