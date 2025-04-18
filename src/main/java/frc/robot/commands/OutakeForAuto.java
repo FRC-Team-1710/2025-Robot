77632b15b3433b2ab8b5be1aecaa6b3a9805d536
Camille-Jones <459cmjo2@students.olatheschools.com>
2025-04-18 07:46:26 -0500

    Revert "bums every last one of them"
    This reverts commit ebe55b5e76dbd502347334e8800bb6a0ea33abc1.

--- src/main/java/frc/robot/commands/OutakeForAuto.java
+++ src/main/java/frc/robot/commands/OutakeForAuto.java
@@ -7,7 +7,6 @@ package frc.robot.commands;
 import com.ctre.phoenix6.swerve.SwerveRequest.RobotCentric;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.superstructure.elevator.Elevator;
 import frc.robot.subsystems.superstructure.manipulator.Manipulator;
@@ -42,14 +41,11 @@ public class OutakeForAuto extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+
     // Logger.recordOutput("Command", "Outtake");
     if (elevator.isAtTarget()) {
       manipulator.runPercent(ManipulatorConstants.outtakeSpeed);
       drivetrain.stop(requestsupplier);
-    } else if (timer.get() > 3) {
-      drivetrain
-          .applyRequest(() -> requestsupplier.withVelocityX(-0.3).withVelocityY(-0.3))
-          .until(() -> timer.get() == 3.5);
     } else {
       elevator.setElevatorPosition(elevator.getMode());
       drivetrain.stop(requestsupplier);
@@ -66,7 +62,6 @@ public class OutakeForAuto extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (Constants.currentMode == Constants.Mode.SIM) return false;
     return ((!manipulator.beam1Broken() && !manipulator.beam2Broken()));
   }
 }
