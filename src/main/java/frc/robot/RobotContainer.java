9bc21dd26d89f655196a60e14e374d582c56e6b6
CARTeR EiLERT <carterc.eilert@gmail.com>
2025-04-18 09:33:07 -0500

    me on the pwayground wheee

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -929,24 +929,24 @@ public class RobotContainer {
 
     grabAlgaeFromFloor
         .onTrue(claw.FLOOR())
+        .whileTrue(new GrabAlgae(claw))
+        .onFalse(claw.IDLE())
+        .and(driver.rightStick())
         .whileTrue(
-            new GrabAlgae(claw)
-                .alongWith(
-                    drivetrain
-                        .applyRequest(
-                            () ->
-                                drive
-                                    .withRotationalRate(
-                                        Constants.MaxAngularRate.times(
-                                            -Units.degreesToRadians(vision.getAlgaeYaw()) * 0.4))
-                                    .withVelocityX(
-                                        MaxSpeed.times(
-                                            -driver
-                                                .customLeft()
-                                                .getY())) // Drive forward with negative Y (forward)
-                                    .withVelocityY(MaxSpeed.times(-driver.customLeft().getX())))
-                        .onlyWhile(driver.rightStick())))
-        .onFalse(claw.IDLE());
+            drivetrain
+                .applyRequest(
+                    () ->
+                        drive
+                            .withRotationalRate(
+                                Constants.MaxAngularRate.times(
+                                    -Units.degreesToRadians(vision.getAlgaeYaw()) * 0.4))
+                            .withVelocityX(
+                                MaxSpeed.times(
+                                    -driver
+                                        .customLeft()
+                                        .getY())) // Drive forward with negative Y (forward)
+                            .withVelocityY(MaxSpeed.times(-driver.customLeft().getX())))
+                .onlyWhile(driver.rightStick()));
 
     previousTarget
         .and(() -> TargetingComputer.targetingControllerOverride ? targetReef.getAsBoolean() : true)
